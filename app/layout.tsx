35842c667569145e8655fb1132d059f8fda6cfc0
miguelbenegasungs <ingindustrialpasantias@gmail.com>
2026-06-03 20:37:39 -0300

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,15 @@ export default function RootLayout({
             <Script
       src="https://www.googletagmanager.com/gtag/js?id=G-5EYLWXVL7K"
       strategy="afterInteractive"
-    />
+    /> 
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x1hokjvmc8");
+</script>
 
     <Script id="google-analytics" strategy="afterInteractive">
       {`
@@ -59,6 +67,8 @@ export default function RootLayout({
         gtag('config', 'G-5EYLWXVL7K');
       `}
     </Script>
+    
+    
       </body>
     </html>
   )
